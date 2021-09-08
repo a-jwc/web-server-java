74cfb4589c1eec886776eee93c786d3c7c4c3779
Alexander Chau <alexanderchau@Alexanders-MacBook-Air.local>
2021-09-07 19:15:49 -0700

    update Webserver.java with header parse

--- WebServer.java
+++ WebServer.java
@@ -1,12 +1,21 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
 
-public class WebServer {
-    public static void main(String[] args) {
+public class test {
 
-    }   
-    
-    /*
+  static String get_req =
+    "GET /index.html HTTP/1.1
+User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)
+Host: www.tutorialspoint.com
+Accept-Language: en-us
+Accept-Encoding: gzip, deflate
+Connection: Keep-Alive";
+
+  public static void main(String[] args) {
+    checkRequest(get_req);
+  }
+
+  /*
         GET /hello.htm HTTP/1.1
         User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)
         Host: www.tutorialspoint.com
@@ -15,7 +24,7 @@ public class WebServer {
         Connection: Keep-Alive
     */
 
-    /* • HTTP_METHOD IDENTIFIER HTTP_VERSION
+  /* • HTTP_METHOD IDENTIFIER HTTP_VERSION
 
         HTTP_HEADERS
 
@@ -26,18 +35,49 @@ public class WebServer {
         • IDENTIFIER is the URI of the resource or the body
         • HTTP_VERSION is the HTTP version being used by the client
         • BODY is an optional payload (the Content-Length header must be present if BODY is present)
-        */
-    public void getRequest(string URI) {
+    */
 
-    }   
+  public static void getRequest(String URI) {}
+  
+  public static void checkRequest(String get_req) {
+    // Split by new lines
+    String reqArr[] = get_req.split("\\r?\\n");
 
-    public void checkRequest(String request) {
-        string http_method = request.split(0, 1);
-        string uri = request.split(1, 2);
-        switch(http_method) {
-            case "GET":
-                getRequest(uri);
-                break;
-        }
+    // Split first line by whitespace
+    String headerArr[] = reqArr[0].split(" ");
+
+    for (int i = 0; i < reqArr.length; i++) {
+      System.out.println(reqArr[i] + "\n");
     }
+
+    // HTTP method string
+    String http_method = headerArr[0];
+    // identifier string
+    String uri = headerArr[1];
+    // HTTP version string
+    String http_version = headerArr[2];
+
+    switch (http_method) {
+      case "GET":
+        System.out.println(
+          "HTTP_Method: " +
+          http_method +
+          "\nURI: " +
+          uri +
+          "\nHTTP_Version: " +
+          http_version +
+          "\n"
+        );
+        getRequest(uri);
+        break;
+      case "POST":
+        break;
+      case "HEAD":
+        break;
+      case "PUT":
+        break;
+      case "DELETE":
+        break;
+    }
+  }
 }
