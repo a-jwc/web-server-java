47a3247241c97567204777698227c4d11c007f9f
a-jwc <chauajw@gmail.com>
2021-09-26 07:20:49 -0700

    finished getters

--- server/config/HtAccess.java
+++ server/config/HtAccess.java
@@ -1,12 +1,40 @@
 package server.config;
 
+import java.io.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class HtAccess {
     private static ConcurrentHashMap<String, String> htAccessMap;
 
-    public HtAccess(ConcurrentHashMap<String, String> htAccess) {
-        htAccessMap = htAccess;
+    public HtAccess(String htAccessPath) {
+        // * Parse and put htAccess items in the hash map
+        try {
+            FileInputStream fis = new FileInputStream(htAccessPath);
+            DataInputStream dis = new DataInputStream(fis);
+            BufferedReader br = new BufferedReader(new InputStreamReader(dis));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            String htArr[];
+            while((line = br.readLine()) != null) {
+                if(!line.startsWith("#") && !line.isBlank()) {
+                    htArr = line.split("\\s+", 2);                    
+                    if(htArr.length == 2) {                        
+                        // System.out.println(mimeTypesMap.size() + " : " + mimeType[0] + " " + mimeType[1]);
+                        htAccessMap.put(htArr[0], htArr[1]);
+                        // System.out.println(Arrays.toString(mimeTypesMap.get(mimeType[0])));
+                        sb.append(line + "\r\n");
+                    } 
+                }
+            }
+            br.close();
+            dis.close();
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("⌛ Successfully read in " + htAccessPath + "\n");
+        }
+
     }
 
     // * Get absolute path of .htpassword
