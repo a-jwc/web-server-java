f0c4f7a035a5d89166fd25493911ab1ead4be10d
a-jwc <chauajw@gmail.com>
2021-09-26 07:20:49 -0700

    finished getters

--- server/config/HtAccess.java
+++ server/config/HtAccess.java
@@ -1,11 +1,31 @@
 package server.config;
 
-import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class HtAccess {
-    Hashtable<String,String> htAccessTable;
+    private static ConcurrentHashMap<String, String> htAccessMap;
 
-    public HtAccess() {
+    public HtAccess(ConcurrentHashMap<String, String> htAccess) {
+        htAccessMap = htAccess;
+    }
 
+    // * Get absolute path of .htpassword
+    public String getAuthUserFile() {
+        return htAccessMap.get("AuthUserFile");
+    }
+
+    // * Only need to support "Basic"
+    public String getAuthType() {
+        return htAccessMap.get("AuthType");
+    }
+
+    // * Get the name provided in the authentication window provided by clients
+    public String getAuthName() {
+        return htAccessMap.get("AuthName");
+    }
+
+    // * Get the user or group that can access a resource ("valid-user")
+    public String getReq() {
+        return htAccessMap.get("Require");
     }
 }
