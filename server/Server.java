6a2c4a1e3f09a31d598e376422d96a9c7d0cf55c
michaelsatumba <mjsatumba@gmail.com>
2021-09-27 15:41:51 -0700

    httpd.conf

--- server/Server.java
+++ server/Server.java
@@ -10,7 +10,7 @@ public class Server {
     private static int port;
     ServerSocket serverSocket;
     ExecutorService threadPool;
-    String httpd_alex = "conf/httpd_alex.conf";
+    String httpd_alex = "conf/httpd.conf";
 
     public Server() {
         // * Instantiate new Configuration object and sets config private members with
@@ -20,13 +20,13 @@ public class Server {
         config.readHttpdConfig();
         // * Get the "Listen" directive from the hash Map member as the listening port
         port = Integer.parseInt(config.getConfigMap().get("Listen"));
-        serverSocket = null;    
+        serverSocket = null;
         // * Initialize the threadpool with 10 threads
         // threadPool = Executors.newFixedThreadPool(10);
         threadPool = Executors.newCachedThreadPool();
     }
 
-    public void start() throws IOException {            
+    public void start() throws IOException {
             try {
                 serverSocket = new ServerSocket(port);
                 System.out.println("✨ Server started. \n" + "📭 Listening for messages on port " + port + "\n");
@@ -44,10 +44,10 @@ public class Server {
             } catch (IOException e) {
                 e.printStackTrace();
                 throw new RuntimeException(e);
-            } 
+            }
             finally {
                 threadPool.shutdown();
-            } 
+            }
     }
 
     public void stop() throws IOException {
